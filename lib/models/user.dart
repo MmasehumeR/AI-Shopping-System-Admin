88a4fc8f0c71161f0fd297b810993b2533f80c87
mmasehume <mmasehu@gmail.com>
2021-10-07 01:54:39 +0200

    users page

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,21 +2,35 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
   static const ID = "uid";
-  static const NAME = "name";
+  static const NAME = "fname";
+  static const SURNAME = "lname";
   static const EMAIL = "email";
+  static const PROVINCE = "province";
+  static const BIRTHDAY = "bday";
 
   String _id;
   String _name;
   String _email;
+  String _surname;
+  String _province;
+  String _birthday;
 
 //  getters
   String get name => _name;
   String get email => _email;
   String get id => _id;
+  String get surname => _surname;
+  String get province => _province;
+  String get birthday => _birthday;
 
   UserModel.fromSnapshot(DocumentSnapshot snapshot) {
     _name = snapshot.data()[NAME];
     _email = snapshot.data()[EMAIL];
     _id = snapshot.data()[ID];
+    // print("id is " + _id);
+    _surname = snapshot.data()[SURNAME];
+    _province = snapshot.data()[PROVINCE];
+    _birthday = snapshot.data()[BIRTHDAY];
+    
   }
 }
